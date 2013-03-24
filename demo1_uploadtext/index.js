eac55782ba76ded74e86925bb5084cbeded83d80
PengEdy <peng.edy@gmail.com>
2013-03-24 12:59:25 +0800

    second commit

--- demo1_uploadtext/index.js
+++ demo1_uploadtext/index.js
@@ -1,11 +1,11 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+var server = require("./server");		//加载server模块
+var router = require("./router");		//加载router模块
+var requestHandlers = require("./requestHandlers"); //加载requestHandler模块
 
-var handle = {}
-handle["/"] = requestHandlers.start;
+var handle = {}					//对不同URL请求的处理
+handle["/"] = requestHandlers.start;		
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
 
-server.start(router.route, handle);
+server.start(router.route, handle);		//服务器启动参数
--- demo1_uploadtext/router.js
+++ demo1_uploadtext/router.js
@@ -1,8 +1,12 @@
+/*Router文件说明
+ *router.js用来处理不同URL的请求
+ *本文件中只有对于异常URL的处理方式，而对于正常业务的URL处理方式则在responseHandler.js中。
+ */
 function route(handle, pathname, response, postData) {
 	console.log("About to route a request for " + pathname);
-	if(typeof handle[pathname] === 'function') {
+	if(typeof handle[pathname] === 'function') {	//如果URL的pathname为正常业务请求，则调用相关函数
 		handle[pathname](response, postData);
-	}else {
+	}else {						//对于异常URL的处理方式
 		console.log("No request handler found for " + pathname);
 		response.writeHead(404, {"Content-Type": "text/plain"});
 		response.write("404 Not Found");

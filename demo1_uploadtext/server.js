3d8a40bfdad533bf772717c72da9cfd5ee451167
PengEdy <peng.edy@gmail.com>
2013-03-24 13:08:24 +0800

    second commit

--- demo1_uploadtext/server.js
+++ demo1_uploadtext/server.js
@@ -1,17 +1,23 @@
+/*server.js文件说明
+ *将data和end事件的回调函数直接放在服务器中，在data事件回调中收集所有POST数据。
+ *当接收到所有数据，触发end事件后，其回调函数调用请求路由，并将数据传递给它。
+ *然后，请求路由再将该数据传给请求处理程序。
+ */
+
 var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
 		var postData = "";
-		var pathname = url.parse(request.url).pathname;
+		var pathname = url.parse(request.url).pathname;			// 从URL中提取出pathname
 		console.log("Request for " + pathname + " received.");
 		request.setEncoding("utf8");
-		request.addListener("data", function (postDataChunk) {
+		request.addListener("data", function (postDataChunk) {		//data的回调函数
 			postData += postDataChunk;
 			console.log("Received POST data chunk " + postDataChunk + ".");
 		});
-		request.addListener("end", function() {
+		request.addListener("end", function() {				//end的回调函数
 			route(handle, pathname, response, postData);
 		});
 	}
